05409737558c27d0eda082afd884a10045582a54
Baruch R. <azulamarillo000@gmail.com>
2024-10-20 23:31:58 -0600

    Group_concat y Json_arrayagg

--- AY18_groupby_Baruch.sql
+++ AY18_groupby_Baruch.sql
@@ -33,7 +33,8 @@ para agrupar el conjunto de resultados por una o más columnas.
 
 /*
 Metadata:
-La tabla `city` tiene registros de ciudades con una llave foránea `CountryCode` que indica el país al que pertenecen.
+-> La tabla `city` tiene registros de ciudades con una llave foránea `CountryCode` que indica el país al que pertenecen.
+-> La tabla `countrylanguage` tiene registros de los idiomas hablados en diferentes países con una llave foránea `CountryCode` que indica el país en cuestión.
 */
 
 /*
@@ -90,3 +91,49 @@ GROUP_CONCAT()
 Función que devuelve un resultado de cadena con valores no nulos concatenados de un grupo.
 Devuelve NULL si no hay valores no nulos.
 */
+
+/*
+Queremos listar los idiomas registrados de cada país en un formato concatenado, agrupados por país.
+*/
+SELECT CountryCode, GROUP_CONCAT(Language) AS Languages
+FROM countrylanguage
+GROUP BY CountryCode
+LIMIT 10;
+-- Limitamos los registros a 10 para reducir los resultados en pantalla.
+
+/*
+Para tener una mejor visualización cuando los resultados son demasiado anchos para la ventana actual,
+terminamos la sentencia con \G en lugar de ;
+Esto hace que los conjuntos de resultados se muestren verticalmente.
+*/
+SELECT CountryCode, GROUP_CONCAT(Language) AS Languages
+FROM countrylanguage
+GROUP BY CountryCode
+LIMIT 10 \G
+-- Limitamos los registros a 10 para reducir los resultados en pantalla.
+
+/*
+La función GROUP_CONCAT() admite las siguientes opciones:
+GROUP_CONCAT(Language ORDER BY Language ASC SEPARATOR ' <-> ')
+Al usar ORDER BY hacemos que el formato concatenado esté ordenado.
+Por default, el separador es ', ', el cual se puede cambiar al especificarlo con SEPARATOR.
+*/
+SELECT CountryCode, GROUP_CONCAT(Language ORDER BY Language ASC SEPARATOR ' <-> ') AS Languages
+FROM countrylanguage
+GROUP BY CountryCode 
+LIMIT 10 \G
+-- Limitamos los registros a 10 para reducir los resultados en pantalla.
+
+/*
+JSON_ARRAYAGG()
+Función que agrega un conjunto de resultados como un único arreglo JSON cuyos elementos consisten en las filas.
+*/
+
+/*
+Queremos obtener un arreglo JSON de todas las ciudades de cada país.
+*/
+SELECT CountryCode, JSON_ARRAYAGG(Name) AS CitiesArray
+FROM city
+GROUP BY CountryCode
+LIMIT 10 \G
+-- Limitamos los registros a 10 para reducir los resultados en pantalla.
