da54e7d09107a46325d41b8783eff9c5fb32916c
Baruch R. <azulamarillo000@gmail.com>
2024-11-11 20:01:05 -0600

    Creación .sql de AY24

---
+++ AY24_procedimientos_almacenados_Baruch.sql
@@ -0,0 +1,11 @@
+/*
+=================================
+Ayudantía 24: Procedimientos almacenados
+Rodrigo Baruch Rivera Rico
+14 noviembre 2024
+=================================
+*/
+
+-- Antes de empezar: cargar la base world y ponerla en uso
+-- \. path\to\script\base_world.sql
+-- \u world
